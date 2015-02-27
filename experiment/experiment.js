1bd39a98d1032a9201829d136d98eab27e2d86a5
kgweisman <kweisman@stanford.edu>
2015-02-26 17:09:10 -0800

    add optional bonus round

--- experiment/experiment.js
+++ experiment/experiment.js
@@ -7,32 +7,34 @@ var chosenCondition = randomElementNR(swatchSets);
 // create experiment object
 var experiment = {
 	trials: chosenCondition.swatchOrder.slice(),
+	bonusTrials: [chosenCondition.swatchOrder[0], chosenCondition.swatchOrder[6], chosenCondition.swatchOrder[11]],
 	dateOfTest: new Date(),
 	condition: chosenCondition.condition.slice(),
 	trialData: [],
 
 	// what happens after completing all trials
 	end: function() {
-		showSlide("bonus");
+		showSlide("finished");
 	},
 
-	// what happens when participant sees a new trial
-	next: function() {
-		if (this.trials.length === 0) {
-			experiment.end();
-		} else {
+	// what happens when participant plays bonus rounds
+	bonus: function() {
 
-			// create place to store data for this trial
+		// set up how to play a bonus trial
+		function playBonus() {
+
+			// create place to store data for this bonus trial
 			var data = {
-				trialNum: (chosenCondition.swatchOrder.length + 1) - this.trials.length,
+				phase: "bonus",
+				trialNum: 4 - experiment.bonusTrials.length,
 				swatch: "",
 				response: "",
-				responseCoded: NaN,
+				responseCoded: "",
 				rt: NaN
-			};
+			}
 
 			// choose random image to display
-			chosenSwatch = randomElementNR(this.trials);
+			var chosenSwatch = randomElementNR(experiment.bonusTrials);
 			data.swatch = chosenSwatch.swatchName;
 
 			// display chosen image
@@ -60,8 +62,82 @@ var experiment = {
 				$('.slide#stage button').unbind().blur();
 				window.scrollTo(0, 0);
 				experiment.next();
-			})
+			});
+		};
 
+		if (experiment.bonusTrials.length === 0) {
+
+			// end study session
+			experiment.end();
+
+		} else if (experiment.bonusTrials.length === 3) {
+
+			// give option of bonus round
+			var chooseBonus = window.confirm("Do you want to play a bonus round?");
+			if (chooseBonus === true) {
+				// if child says yes...
+				playBonus();
+
+			} else {
+			// if child says no...
+
+			experiment.end();
+			}			
+
+		} else {
+			playBonus();
+		}
+	},
+
+	// what happens when participant sees a new trial
+	next: function() {
+		if (this.trials.length === 0) {
+
+			// move on to bonus round
+			experiment.bonus();
+
+		} else {
+
+			// create place to store data for this trial
+			var data = {
+				phase: "animal",
+				trialNum: (chosenCondition.swatchOrder.length + 1) - this.trials.length,
+				swatch: "",
+				response: "",
+				responseCoded: NaN,
+				rt: NaN
+			};
+
+			// choose random image to display
+			var chosenSwatch = randomElementNR(this.trials);
+			data.swatch = chosenSwatch.swatchName;
+
+			// display chosen image
+			$('.slide#stage img').attr("src", chosenSwatch.imageSource);
+
+			// show trial
+			showSlide("stage");
+
+			// record response and rt
+			var startTime = (new Date()).getTime();
+
+			var clickHandler = function(event) {
+				var endTime = (new Date()).getTime();
+				data.rt = endTime - startTime;
+				experiment.trialData.push(data);
+			};
+
+			$('.slide#stage button').click(function() {
+				// record response
+				data.response = $(this).attr('id');
+				data.responseCoded = parseFloat($(this).attr('value'));
+
+				// end trial
+				clickHandler();
+				$('.slide#stage button').unbind().blur();
+				window.scrollTo(0, 0);
+				experiment.next();
+			});
 		}
 	}
 };
