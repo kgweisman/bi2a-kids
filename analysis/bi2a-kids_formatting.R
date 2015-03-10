57c24adb313c3d3d81cbb94efb4af6526931c90a
kgweisman <kweisman@stanford.edu>
2015-03-10 09:33:16 -0700

    initialize analysis script for bi2a-kids pilot

---
+++ analysis/bi2a-kids_formatting.R
@@ -0,0 +1,16 @@
+# --- PRELIMINARIES -----------------------------------------------------------
+
+# libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(lme4)
+
+# clear environment
+rm(list=ls())
+
+# --- READING IN DATA OBJECTS -------------------------------------------------
+
+# set working directory for india
+setwd("/Users/kweisman/Documents/Research (Stanford)/Projects/BI2A/bi2a-kids/data/")
+
